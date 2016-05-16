f3947c45b782ed2177ba2e9cd27c7ec5a65cf600
Simon Fankhauser <simu@snowplow.ch>
2016-05-16 13:42:33 +0200

    Added decryption script (basics).

---
+++ fisel_decr.py
@@ -0,0 +1,12 @@
+import hashdeep
+import obfuscate
+import package
+
+#hashdeep_out_file = "file_hashes.out"
+
+package.unpackage()
+
+#hashdeep.hashdeep(hashdeep_out_file)
+#obfuscate.obfuscate(hashdeep_out_file)
+
+
--- package.py
+++ package.py
@@ -9,36 +9,82 @@ def package():
         print 'ERROR: Encryption key cannot be read from environment variable ' + env_var_name
         return
 
-    gpg_cmd = 'gpg --passphrase ' + encr_key + ' -c --no-use-agent --cipher-algo AES256 --symmetric '
+    gpg_cmd = 'gpg --passphrase ' + encr_key + ' --no-use-agent --cipher-algo AES256 --symmetric '
 
     for i in os.listdir(os.getcwd()):
-        if os.path.isdir(i):
 
+        if os.path.isdir(i):
             # Create tar ball for each directory
             exec_command('tar -cf ' + i + '.tar ' + i)
-
             # Remove original directory
             exec_command('rm -rf ' + i)
-
             # Symmetrically encrypt tar ball using AES-256
             exec_command(gpg_cmd + i + '.tar')
-
             # Remove unencrypted tar ball
             exec_command('rm ' + i + '.tar')
 
         else:
             # Symmetrically encrypt file using AES-256
             exec_command(gpg_cmd + i)
-
             # Remove unencrypted tar ball 
             exec_command('rm ' + i)
 
         print 'Encrypted ' + i
 
-    print "Finished."
+    print "Finished encryption."
+
+
+directory_map_name = 'directory_mapping.out'
+file_hashes_name = 'file_hashes.out'
+
+def unpackage():
+
+    decr_key = os.environ.get(env_var_name)
+    if not decr_key:
+        print 'ERROR: Decryption key cannot be read from environment variable ' + env_var_name
+        return
+
+    gpg_cmd = 'gpg -d --passphrase ' + decr_key + ' --no-use-agent --output '
+
+    # decrypt directory mapping
+    exec_command(gpg_cmd + directory_map_name + ' ' + directory_map_name + '.gpg')
+    exec_command('rm ' + directory_map_name + '.gpg')
+
+    # decrypt file hashes
+    exec_command(gpg_cmd + file_hashes_name + ' ' + file_hashes_name + '.gpg')
+    exec_command('rm ' + file_hashes_name + '.gpg')
+
+    for i in os.listdir(os.getcwd()):
+        if i.endswith('.tar.gpg'):
+            filename = i[:-4]
+            # Decrypt file using AES-256
+            exec_command(gpg_cmd + filename + ' ' + i)
+            # Remove encrypted tar ball 
+            exec_command('rm ' + i)
+            # Extract tar ball
+            exec_command('tar -xf ' + filename)
+            # Remove tar ball
+            exec_command('rm ' + filename)            
+            print 'Decrypted ' + i
+
+    with open(directory_map_name) as dirmap:
+        for line in dirmap:
+            rename = line.split(' -> ')
+            # Restore original folder name
+            exec_command_args(['mv', rename[1].strip(), rename[0].strip()])
+
+    # Extract root files to root
+    #exec_command('mv root_files/* .')
+    #exec_command('rm -d root_files')
+
+    print "Finished decryption."
+
 
 def exec_command(command):
+    exec_command_args(command.split(' '))
+
+def exec_command_args(args):
     from subprocess import call
     #print "Executing shell command: " + command
-    call(command.split(' '))
+    call(args)
 
